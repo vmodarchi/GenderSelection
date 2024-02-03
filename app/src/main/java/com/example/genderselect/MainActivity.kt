4e732db0ba688d864bd097ec4df3e44313e10391
Vladimir Modirchi <vladimirmodirchi@gmail.com>
2024-02-03 12:33:42 +0200

    Initial Commit

--- app/src/main/java/com/example/genderselect/MainActivity.kt
+++ app/src/main/java/com/example/genderselect/MainActivity.kt
@@ -2,6 +2,7 @@
 package com.example.genderselect
 
 import android.app.Activity
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -30,14 +31,21 @@ class MainActivity : Activity() {
                 val radioButton = findViewById<RadioButton>(selectedId)
                 val gender = radioButton.text.toString()
 
-                // Display the result on the screen
-                tvResult.text = "Selected Gender: $gender"
-
                 // Log the result in the console
                 Log.d("GenderSelectionApp", "Selected Gender: $gender")
+
+                // Display the result on the screen with colored text
+                if (gender.equals("Male", true)) {
+                    tvResult.text = "Selected Gender: $gender"
+                    tvResult.setTextColor(Color.BLUE)
+                } else if (gender.equals("Female", true)) {
+                    tvResult.text = "Selected Gender: $gender"
+                    tvResult.setTextColor(Color.parseColor("#FF4081")) // Pink color
+                }
             } else {
                 // Handle the case where no gender is selected
                 tvResult.text = "Please select a gender."
+                tvResult.setTextColor(Color.BLACK) // Reset text color
             }
         }
     }
